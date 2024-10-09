58efa24e69191bd1732114edcec71ec70fd7c8dc
Renato <rverissimo.dev@gmail.com>
2024-10-09 16:48:23 -0300

    fix: retirando importações e códigos não utilizados da classe Main

--- XMLTester/src/main/java/org/example/Main.java
+++ XMLTester/src/main/java/org/example/Main.java
@@ -4,16 +4,9 @@ import org.example.abstractions.IESocialTotalizador;
 import org.example.factory.ESocialTotalizadorFactory;
 import org.example.infrastructure.FileContainer;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-
-        //File xmlFile = new File("src/main/resources/totalizador-INSS-teste.xml");
-        //"src/main/resources/totalizador-INSS-teste.xml"
 
         FileContainer fileContainer = new FileContainer();
         String xml = fileContainer.getXmlAsString("src/main/resources/totalizador-INSS-teste.xml");
