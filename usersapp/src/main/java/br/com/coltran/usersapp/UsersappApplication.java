72873901160be2860f1109278b68a19193565e53
Renato <rverissimo.dev@gmail.com>
2024-10-14 17:18:11 -0300

    novo projeto: adicionando um projeto MVC simples usando o Spring MVC, Spring Boot 2.7, Java 11 e H2

---
+++ usersapp/src/main/java/br/com/coltran/usersapp/UsersappApplication.java
@@ -0,0 +1,13 @@
+package br.com.coltran.usersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersappApplication.class, args);
+	}
+
+}
---
+++ usersapp/src/main/java/br/com/coltran/usersapp/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.coltran.usersapp.repositories;
+
+import br.com.coltran.usersapp.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
