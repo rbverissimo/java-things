f5e8411c44020e47df5ecdcd532dc72856c5e71b
Renato <rverissimo.dev@gmail.com>
2024-10-16 17:29:44 -0300

    adding a new project: the math tester. we never know when we need one

---
+++ math-tester/src/main/java/br/com/coltran/Main.java
@@ -0,0 +1,11 @@
+package br.com.coltran;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(2 % 2);
+        System.out.println(3 % 2);
+        System.out.println(4 % 2);
+        System.out.println(5 % 2);
+
+    }
+}
