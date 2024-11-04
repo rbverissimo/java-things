cb69b1d1c613926f975ae1c3cf2327fcf7152576
Renato <rverissimo.dev@gmail.com>
2024-11-04 11:29:57 -0300

    Iniciando um novo projeto simples com o intuito conhecer mais sobre o H2, o Spring Security, Thymeleaf e o desenvolvimento rápido de software

---
+++ farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/FarmacinhappApplication.java
@@ -0,0 +1,13 @@
+package br.com.coltran.farmacinhapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmacinhappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmacinhappApplication.class, args);
+	}
+
+}
