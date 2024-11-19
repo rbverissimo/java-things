ebb3bf438222bbf61dfdd08a58d3bade4e9bd618
Renato <rverissimo.dev@gmail.com>
2024-11-19 17:04:29 -0300

    Adicionando a entidade das roles (seguindo o padrão RBAC) no contexto do Spring Security

--- farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/security/domain/Role.java
+++ farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/security/domain/Role.java
@@ -1,5 +1,44 @@
 package br.com.coltran.farmacinhapp.security.domain;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
 }
