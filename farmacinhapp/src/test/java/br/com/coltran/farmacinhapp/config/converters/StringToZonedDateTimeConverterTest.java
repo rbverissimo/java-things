6832d79a1b8ccaf9d30b9fb2b72bdeddcbe1e3d9
Renato <rverissimo.dev@gmail.com>
2025-01-29 12:25:47 -0300

    I'm finally learning how to test stuff in Spring

--- farmacinhapp/src/test/java/br/com/coltran/farmacinhapp/config/converters/StringToZonedDateTimeConverterTest.java
+++ farmacinhapp/src/test/java/br/com/coltran/farmacinhapp/config/converters/StringToZonedDateTimeConverterTest.java
@@ -1,6 +1,8 @@
 package br.com.coltran.farmacinhapp.config.converters;
 
+import br.com.coltran.farmacinhapp.utils.ZonedBrasilTime;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
@@ -10,10 +12,13 @@ import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+
+@SpringBootTest(classes = {StringToZonedDateTimeConverter.class, ZonedBrasilTime.class})
 public class StringToZonedDateTimeConverterTest {
 
-    private final StringToZonedDateTimeConverter converter = new StringToZonedDateTimeConverter();
+    @Autowired
+    private StringToZonedDateTimeConverter converter;
+
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     private final ZoneId brZoneId = ZoneId.of("America/Sao_Paulo");
 
