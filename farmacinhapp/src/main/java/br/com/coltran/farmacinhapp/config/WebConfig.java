7be0db9953f12a8e33eb84c042d6305cfae4d18f
Renato <rverissimo.dev@gmail.com>
2025-03-12 17:29:50 -0300

    fixing the WebMvcConfigurer

--- farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/config/WebConfig.java
+++ farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/config/WebConfig.java
@@ -4,20 +4,12 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    /*@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/webjars/**")
-                .addResourceLocations("/webjars/");
-    }*/
 
     @Bean
     public FilterRegistrationBean<Filter> hiddenHttpMethodFilter(){
