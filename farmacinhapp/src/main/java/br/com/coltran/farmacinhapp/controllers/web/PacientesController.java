f9a7633b5b1367dbb011ca6f52e4f1fef0021191
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2025-03-20 16:31:31 -0300

    Finally understanding and applying templating a proper way integrating both my custom JS and CSS, Vue, Bootstrap and Thymeleaf!!

--- farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/controllers/web/PacientesController.java
+++ farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/controllers/web/PacientesController.java
@@ -2,9 +2,15 @@ package br.com.coltran.farmacinhapp.controllers.web;
 
 import br.com.coltran.farmacinhapp.controllers.ControllerCommons;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/pacientes")
 public class PacientesController extends ControllerCommons {
+
+    @GetMapping("/{id}")
+    public String index(){
+        return "pacientes/index";
+    }
 }
