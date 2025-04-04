ebd856dc8727be131d35c42402fc0ffc11f877cd
Renato <rverissimo.dev@gmail.com>
2025-04-04 11:28:31 -0300

    Implementing access of CSRF Token, but also resolving some bugs and getting DELETE from the API working

--- farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/repositories/GramaturaRepository.java
+++ farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/repositories/GramaturaRepository.java
@@ -4,9 +4,12 @@ import br.com.coltran.farmacinhapp.domain.Gramatura;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface GramaturaRepository extends JpaRepository<Gramatura, Long> {
@@ -14,7 +17,9 @@ public interface GramaturaRepository extends JpaRepository<Gramatura, Long> {
     @Query("SELECT g FROM Gramatura g JOIN g.remedios r WHERE r.id=:remedioId")
     Page<Gramatura> findByRemedioId(@Param("remedioId") Long remedioId, Pageable pageable);
 
-    @Query("DELETE FROM Gramatura g JOIN g.remedios r WHERE g.id=:gramaturaId AND r.id=:remedioId")
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM remedios_gramaturas WHERE gramatura_id=:gramaturaId AND remedio_id=:remedioId", nativeQuery = true)
     void deleteGramaturaFromRemedio(@Param("gramaturaId") long gramaturaId, @Param("remedioId") long remedioId);
 
 
---
+++ farmacinhapp/src/main/resources/static/js/token.js
@@ -0,0 +1,2 @@
+export const csrftoken = document.getElementById('app').dataset.csrfToken;
+export const csrfheader = document.getElementById('app').dataset.csrfHeader;
