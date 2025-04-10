785e2393b0fce80cdfddffbc9e721bf5b6a23d5d
Renato <rverissimo.dev@gmail.com>
2025-04-10 09:45:30 -0300

    solved some bugs regarding axios and vue

--- farmacinhapp/src/main/resources/static/js/requests.js
+++ farmacinhapp/src/main/resources/static/js/requests.js
@@ -1,12 +1,12 @@
-import { axios } from '/webjars/axios/1.8.4/dist/axios.min.js';
-import { csrfHeader, csrfToken } from '/js/token.js';
+import  axios  from '/webjars/axios/1.8.4/dist/esm/axios.min.js';
+import { csrfheader, csrftoken } from '/js/token.js';
 
 export async function post(url, data){
 
     try {
         const response = await axios.post(url, data, {
             headers: {
-                [csrfHeader]: csrfToken
+                [csrfheader]: csrftoken
             }
         });
         if(response.status >= 200 && response.status < 300) return response.data;
