e8c1365331b2bcfebf59b4b361df3d0e40db0310
Renato <rverissimo.dev@gmail.com>
2025-04-22 15:43:42 -0300

    Implementing forms for the user

--- farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/controllers/web/UsuariosController.java
+++ farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/controllers/web/UsuariosController.java
@@ -2,6 +2,7 @@ package br.com.coltran.farmacinhapp.controllers.web;
 
 import br.com.coltran.farmacinhapp.controllers.ControllerCommons;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UsuariosController extends ControllerCommons {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+
+        model.addAttribute("username", "Teste");
+
         return "usuarios/index";
     }
 }
