eb678142f2d4753acfa96ad3bf10d4c252216c57
Renato <rverissimo.dev@gmail.com>
2025-05-05 12:41:24 -0300

    Implementing index page new features

--- farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/controllers/web/MainController.java
+++ farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/controllers/web/MainController.java
@@ -1,9 +1,16 @@
 package br.com.coltran.farmacinhapp.controllers.web;
 
 import br.com.coltran.farmacinhapp.controllers.ControllerCommons;
+import br.com.coltran.farmacinhapp.controllers.web.dto.RemedioCatalogoDTO;
+import br.com.coltran.farmacinhapp.domain.Farmacia;
+import br.com.coltran.farmacinhapp.domain.Remedio;
+import br.com.coltran.farmacinhapp.domain.valueobjects.RemedioIndexVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController extends ControllerCommons {
@@ -15,7 +22,17 @@ public class MainController extends ControllerCommons {
 
     @GetMapping("/")
     public String home(Model model){
+
+        Set<RemedioIndexVO> remedios = authService.usuarioLogado()
+                        .getFarmacias().stream()
+                        .flatMap(f -> f.getRemedios().stream()
+                                .map(r -> { return new RemedioIndexVO.Builder().buildFromModel(r);}))
+                .collect(Collectors.toSet());
+
         model.addAttribute("usuario", authService.usuarioLogado());
+        model.addAttribute("remedios", remedios);
+
+
         return "index";
     }
 }
