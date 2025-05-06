c646e6b91b003c70d01f2f4663acff50064599e7
Renato <rverissimo.dev@gmail.com>
2025-05-06 13:58:51 -0300

    Adding more features to the main page

--- farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/controllers/web/FarmaciasController.java
+++ farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/controllers/web/FarmaciasController.java
@@ -10,7 +10,6 @@ import br.com.coltran.farmacinhapp.utils.Colecoes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
