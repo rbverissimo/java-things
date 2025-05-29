289ea0dc6696c503b1768554371f7d84ba4fe1a0
Renato <rverissimo.dev@gmail.com>
2025-05-29 16:43:43 -0300

    refactor(duplicate catalogo): query to pa: no longer returns duplicates

--- farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/repositories/RemedioRepository.java
+++ farmacinhapp/src/main/java/br/com/coltran/farmacinhapp/repositories/RemedioRepository.java
@@ -30,7 +30,7 @@ public interface RemedioRepository extends JpaRepository<Remedio, Long> {
     @Query("SELECT r from Remedio r JOIN r.farmacia f JOIN f.users u WHERE u.id=:userId ORDER BY r.id DESC")
     Page<Remedio> findAllRemedioPageByUser(@Param("userId") Long userId, Pageable pageable);
 
-    @Query("SELECT r from Remedio r JOIN r.gramaturas g WHERE LOWER(g.principioAtivo) LIKE LOWER(CONCAT(:pa, '%'))")
+    @Query("SELECT DISTINCT r from Remedio r JOIN r.gramaturas g WHERE LOWER(g.principioAtivo) LIKE LOWER(CONCAT(:pa, '%'))")
     Page<Remedio> findRemedioByPrincipioAtivo(@Param("pa") String principioAtivo, Pageable pageable);
 
     @Modifying
