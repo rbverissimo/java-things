067bb7df3a8bb041d27b88c6640a418e3dc59b10
Renato <rverissimo.dev@gmail.com>
2025-06-09 17:12:07 -0300

    feat(ui): start dark mode implementation

--- farmacinhapp/src/main/resources/static/js/basics.js
+++ farmacinhapp/src/main/resources/static/js/basics.js
@@ -1,3 +1,11 @@
+const themeIcon = document.getElementById('themeModeIcon');
+
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('ano-atual-footer').innerText = new Date().getFullYear();
+
+    themeIcon.onclick = () => {
+        themeIcon.textContent = themeIcon.textContent === 'light_mode' ? 'dark_mode' : 'light_mode';
+        const theme = themeIcon.textContent.includes('light') ? 'light' : 'dark';
+        document.body.setAttribute('data-theme', theme);
+    }
 });
