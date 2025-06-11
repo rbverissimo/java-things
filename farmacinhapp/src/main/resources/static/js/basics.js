327760182c42b596d9d6d1bd403e5bbce6b7d7dd
Renato <rverissimo.dev@gmail.com>
2025-06-11 17:23:37 -0300

    fix(ui): reposition of the dark mode toggle

--- farmacinhapp/src/main/resources/static/js/basics.js
+++ farmacinhapp/src/main/resources/static/js/basics.js
@@ -4,8 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('ano-atual-footer').innerText = new Date().getFullYear();
 
     themeIcon.onclick = () => {
-        themeIcon.textContent = themeIcon.textContent === 'light_mode' ? 'dark_mode' : 'light_mode';
-        const theme = themeIcon.textContent.includes('light') ? 'light' : 'dark';
-        document.body.setAttribute('data-theme', theme);
+        //document.body.setAttribute('data-theme', theme);
     }
 });
